61b3cbac0ba44633f7b289d58955d120a6198bb2
ESHUshri202 <eshushrivastava1106@gmail.com>
2024-10-05 23:15:05 +0530

    footer work

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Login.css'
-
+import logo from '../../assets/logo.png'
 const Login = () => {
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+        <img src={logo} alt='' className='login-logo' />
+        <div className='logo-form'>
+          <h1>Sign Up</h1>
+          <form>
+            <input type='text' placeholder='Your name' />
+            <input type='email' placeholder='email' />
+            <input type='password' placeholder='password' />
+            <button>Sign Up</button>
+            <div className='form-help'>
+              <div className='remenber'>
+              <input type='checkbox'/>
+              <label htmlFor=''>Remenber Me</label>
+              </div>
+              <p>Need Help?</p>
+            </div>
+          </form>
+        </div>
+    </div>
   )
 }
 
