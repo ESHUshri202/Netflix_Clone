a09135adada29f7d6d220a424f3ab167af0ea5da
ESHUshri202 <eshushrivastava1106@gmail.com>
2024-10-05 22:22:26 +0530

    netflix

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,32 @@
+import React, {useEffect, useRef} from 'react'
+import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
+
+
+const TitleCards = ({title,category}) => {
+
+    const cardsRef = useRef();
+    const handleWheel = (event) =>{
+        event.preventDefault();
+        cardsRef.current.scrollLeft += event.deltaX;
+    }
+    useEffect(()=>{
+        cardsRef.current.addEventListener('wheel', handleWheel)
+    },[])
+
+   return (
+    <div className='titlecards'>
+        <h2>{title?title:"Popular on Netflix"}</h2>
+        <div className='card-list' ref={cardsRef}>
+            {cards_data.map((card,index) =>{
+                return <div className='card' key={index}>
+                    <img src={card.image} alt=''/>
+                    <p>{card.name}</p>
+                </div>
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default TitleCards
