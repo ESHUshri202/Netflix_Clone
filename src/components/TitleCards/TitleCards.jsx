bb47f03065a024d149fbe1af9817d19b634c7fdc
ESHUshri202 <eshushrivastava1106@gmail.com>
2024-10-06 12:05:41 +0530

    video page

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,32 +1,52 @@
-import React, {useEffect, useRef} from 'react'
-import './TitleCards.css'
-import cards_data from '../../assets/cards/Cards_data'
+import React, { useEffect, useRef, useState } from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
+import { Link } from "react-router-dom";
+const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
+  const cardsRef = useRef();
 
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YzIzNzRhNmEzYTAyZDJhYmQ5MjI1MzAzOWNhMTQyNyIsIm5iZiI6MTcyODE1OTY1OS4wMTU4NzYsInN1YiI6IjY3MDE5ZTI1OWViZWExOTAwNmY4NzJhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8cdujHMTA1f5PEqnMcYL2goSjYqHWVdJ49rVmtOqGn8",
+    },
+  };
 
-const TitleCards = ({title,category}) => {
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaX;
+  };
+  useEffect(() => {
+    fetch(
+    `https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`,
+    options).then((response) => response.json())
+    .then((response) => setApiData(response.results))
+    .catch((err) => console.error(err));
 
-    const cardsRef = useRef();
-    const handleWheel = (event) =>{
-        event.preventDefault();
-        cardsRef.current.scrollLeft += event.deltaX;
-    }
-    useEffect(()=>{
-        cardsRef.current.addEventListener('wheel', handleWheel)
-    },[])
-
-   return (
-    <div className='titlecards'>
-        <h2>{title?title:"Popular on Netflix"}</h2>
-        <div className='card-list' ref={cardsRef}>
-            {cards_data.map((card,index) =>{
-                return <div className='card' key={index}>
-                    <img src={card.image} alt=''/>
-                    <p>{card.name}</p>
-                </div>
-            })}
-        </div>
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  },[])
+  
+  return (
+    <div className="titlecards">
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {apiData.map((card, index) => {
+          return (
+            <Link to={`/player/${card.id}`} className="card" key={index}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path}
+                alt=""
+              />
+              <p>{card.original_title}</p>
+            </Link>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TitleCards
+export default TitleCards;
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, {useEffect,useState,useParams} from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 const Player = () => {
+
+  const {id} = useParams();
+
+  const [apiData, setApiData] = useState({
+    name:'',
+    key:'',
+    published_at: '',
+    typeof: ''
+  });
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YzIzNzRhNmEzYTAyZDJhYmQ5MjI1MzAzOWNhMTQyNyIsIm5iZiI6MTcyODE1OTY1OS4wMTU4NzYsInN1YiI6IjY3MDE5ZTI1OWViZWExOTAwNmY4NzJhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8cdujHMTA1f5PEqnMcYL2goSjYqHWVdJ49rVmtOqGn8'
+    }
+  };
+  
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+  .then(response => response.json())
+  .then(response => setApiData(response.results[0]))
+  .catch(err => console.error(err));
+
+  },[])
+
+
+  
+
+
   return (
-    <div className='player'>Player</div>
+    <div className='player'>
+      <img src={back_arrow_icon} alt=''/>
+      <iframe width='90%' height='90%'
+      src={`https://www.youtube.com/embed/${apiData.key}`}
+      title='trailer' frameBorder= '0' allowFullScreen></iframe>
+      <div className='player-info'>
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
   )
 }
 
